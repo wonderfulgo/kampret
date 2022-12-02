d1ee28a78fbbb8b828e0644be58effe39af45b44
wonderfulgo <83055997+wonderfulgo@users.noreply.github.com>
2022-12-02 17:11:13 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -27,13 +27,14 @@ cp /etc/passwd backup/
 cp /etc/group backup/
 cp /etc/shadow backup/
 cp /etc/gshadow backup/
-#cp -r /etc/wireguard backup/wireguard
+cp -r /etc/wireguard backup/wireguard
 #cp /etc/ppp/chap-secrets backup/chap-secrets
 #cp /etc/ipsec.d/passwd backup/passwd1
 #cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
 cp -r /var/lib/premium-script/ backup/premium-script
 #cp -r /home/sstp backup/sstp
 cp -r /etc/v2ray backup/v2ray
+cp -r /etc/nginx backup/nginx
 cp -r /etc/sweet backup/sweet
 #cp -r /etc/trojan backup/trojan
 #cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
